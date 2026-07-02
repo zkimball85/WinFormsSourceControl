8c26df303b642e800c13d4638cbe62f1a7e84179
Zac Kimball <z.kimball85@gmail.com>
2026-07-02 12:56:09 -0700

    Update form size and reformat resource XML comments
    Updated Form1.Designer.cs to set new form size (1448x820), assign the Name property, and explicitly call SuspendLayout/ResumeLayout. Reformatted XML comments in Form1.resx for improved readability; no resource data was changed.

--- WinFormsSourceControl/Form1.Designer.cs
+++ WinFormsSourceControl/Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1448, 820);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
